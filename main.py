79e6b6a78f778d18f440b12557ee6f7abc39a480
zeppdev <zepp.dev@gmail.com>
2018-03-13 21:45:12 +0100

    Commit changes to main.py for merge

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from models.RandomForestModel import RandomForestModel
 from models.SVMModel import SVMModel
 from input_output.TitanicSaver import TitanicSaver
 
+
 class Pipeline(object):
-    def __init__(self):
+    def __init__(self, loader=TitanicLoader, preprocessor=TitanicPreprocessor, features=TitanicFeatures,
+                 models=[RandomForestModel], saver=TitanicSaver):
         parser = argparse.ArgumentParser()
         self.args = parser.parse_args()
         self.params = None
@@ -17,31 +19,30 @@ class Pipeline(object):
         self.training_data_file = "Data/train.csv"
         self.test_data_file = "Data/test.csv"
 
-        self.loader = TitanicLoader()
-        self.preprocessor = TitanicPreprocessor()
-        self.features = TitanicFeatures()
-        self.models = [RandomForestModel(self.params)]
-        self.saver = TitanicSaver()
+        self.loader = loader()
+        self.preprocessor = preprocessor()
+        self.features = features()
+
+        self.models = [m(self.params) for m in models]
+        self.saver = saver()
 
     def run(self):
         # load data. Test_labels are PassengerIds which we need to save for the submission
-        x_train, y_train, x_test, test_labels = self.loader.load_split(training_data_file=self.training_data_file, test_data_file=self.test_data_file)
+        x_train, y_train, x_test, test_labels = self.loader.load_split(training_data_file=self.training_data_file,
+                                                                       test_data_file=self.test_data_file)
 
         # preprocess the data and do feature engineering. We just add all features
-        [x_train, x_test] = self.preprocessor.preprocess_datasets( [x_train, x_test] )
-        [x_train, x_test] = self.features.engineer_features_multiple_ds( [x_train, x_test] )
-
+        [x_train, x_test] = self.preprocessor.preprocess_datasets([x_train, x_test])
+        [x_train, x_test] = self.features.engineer_features_multiple_ds([x_train, x_test])
 
         # train all the models
         for model in self.models:
-
             # Check which features are optimal for this model, and train the model with them
             model.feature_selection(x_train, y_train)
             model.train(x_train, y_train, self.model_params)
 
             # TODO: save cv validation performance or something
             # model.acc = ..
-
 
         # TODO: do ensemble learning with the models / select best model?
         bestModel = self.models[0]
@@ -50,7 +51,9 @@ class Pipeline(object):
         bestModel.test(x_test, test_labels)
         self.saver.save_predictions(bestModel.predictions, 'submission.csv')
 
+        return bestModel
 
 
 if __name__ == '__main__':
-    Pipeline().run()
+    Pipeline(loader=TitanicLoader, preprocessor=TitanicPreprocessor, features=TitanicFeatures,
+                 models=[RandomForestModel], saver=TitanicSaver).run()
